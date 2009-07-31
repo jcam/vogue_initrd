ccb77043cb3336284422b6676a0b111028ce611b
Jesse Campbell <hikeit@gmail.com>
2009-07-31 03:10:17 -0700

    Give modem dialing over to the RIL

--- initrd/toggle.sh
+++ initrd/toggle.sh
@@ -1,17 +1,12 @@
 #!/bin/sh
 
-modem_log()
-{
-        /bin/echo -e "${@}" >> /smodem/ppp.temp.log
-}
-
 if [ -e /smodem/working ]; then
 	/bin/echo "setupmodem.sh is not sleeping yet..."
 	/bin/echo ""
 	/bin/echo "Please wait a few seconds before"
 	/bin/echo "running the toggle again"
 	exit 1
-elif [ "`/bin/ifconfig | /bin/grep ^ppp0`" = "" ]; then
+elif [ "`cat /smodem/status`" = "0" ]; then
 	# Turn ppp connection on
 	modem_log "Initiating ppp connection..."
 	/bin/echo "on" > /smodem/control
